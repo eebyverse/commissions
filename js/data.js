c36cb7bcfa13e105091122df550c53fa800a1329
eebyverse <eebyverse@gmail.com>
2025-02-23 22:36:05 +0000

    Update data.js

--- js/data.js
+++ js/data.js
@@ -13,8 +13,22 @@ var data = {
 			hidden: true
 		},
 		{
+			// Evo | Edelcaiah Kiss
+			src: ["https://cdn.imgchest.com/files/7w6c2n6va3y.png"],
+			thumbnail: "https://cdn.imgchest.com/files/yxkczxvqvw7.png",
+			title: "eee",
+			artist: "Evomanaphy",
+			artist_url: "https://bsky.app/profile/evomanaphy.bsky.social",
+			art_url: null,
+			desc: "test",
+			date_str: "Sep 2021",
+			tags: ["sfw", "micaiah", "edelgard"],
+			hidden: false
+		},
+		{
+			// Fusenryo | Eeby + Edelcaiah Kiss
 			src: ["https://cdn.imgchest.com/files/ye3c2ow63z4.png"],
-			thumbnail: "https://cdn.imgchest.com/files/7mmc9gqjxq7.png",
+			thumbnail: "https://cdn.imgchest.com/files/7pjcqmbkxd7.png",
 			title: "eee",
 			artist: "fusenryo2",
 			artist_url: "https://skeb.jp/@fusenryo2",
@@ -22,6 +36,32 @@ var data = {
 			desc: "test",
 			date_str: "Nov 2024",
 			tags: ["sfw", "micaiah", "edelgard", "eeby"],
+			hidden: false
+		},
+		{
+			// Fusenryo | Edelcaiah Kiss
+			src: ["https://cdn.imgchest.com/files/ye3c2owd994.png"],
+			thumbnail: "https://cdn.imgchest.com/files/46acqv8gkg7.png",
+			title: "eee",
+			artist: "fusenryo2",
+			artist_url: "https://skeb.jp/@fusenryo2",
+			art_url: null,
+			desc: "test",
+			date_str: "Oct 2024",
+			tags: ["sfw", "micaiah", "edelgard"],
+			hidden: false
+		},
+		{
+			// Gale | Edelcaiah
+			src: ["https://cdn.imgchest.com/files/4z9cv3jw6b7.png"],
+			thumbnail: "https://cdn.imgchest.com/files/7mmc9gqjxq7.png",
+			title: "eee",
+			artist: "Gale",
+			artist_url: "https://x.com/sampleduckling",
+			art_url: "https://x.com/sampleduckling/status/1683784135322771458",
+			desc: "test",
+			date_str: "Nov 2024",
+			tags: ["sfw", "micaiah", "edelgard"],
 			hidden: false
 		},
 		{
