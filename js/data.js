76f9d227467b0da4f04253ae2cf1993839c40f9f
eebyverse <eebyverse@gmail.com>
2025-07-13 14:32:45 +0100

    Update data.js

--- js/data.js
+++ js/data.js
@@ -162,7 +162,7 @@ var data = {
 			title: "TBD",
 			artist: "arikindows10",
 			artist_url: "https://www.pixiv.net/en/users/658220",
-			art_url: null",
+			art_url: null,
 			desc: "TBD<br/><br/>Gift for <strong><a href='https://bsky.app/profile/impulse96.bsky.social'>Impulse96</a></strong>.",
 			date_str: "Jul 2025",
 			tags: ["nsfw", "anna", "other", "gifts_given"],
