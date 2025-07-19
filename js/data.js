b5abd476c4e657b5ab5c2bb69c8cd7746f263311
eebyverse <eebyverse@gmail.com>
2025-07-19 23:19:11 +0100

    Update data.js

--- js/data.js
+++ js/data.js
@@ -2453,11 +2453,11 @@ var data = {
 			// LazzleDazzler | Anna and Henriette
 			src: ["https://cdn.imgchest.com/files/739cxkwzqr7.png"],
 			thumbnail: "https://cdn.imgchest.com/files/ye3c2g6vkv4.png",
-			title: "TBD",
+			title: "All That Glitters",
 			artist: "LazzleDazzler",
 			artist_url: "https://bsky.app/profile/did:plc:32x5qhojucad7wyl5tsbksqb",
-			art_url: null,
-			desc: "TBD<br/><br/>Gift fr <strong><a href='https://bsky.app/profile/aidenr0.bsky.social'>Spindle</a></strong>.",
+			art_url: "https://bsky.app/profile/exaltedbrand.bsky.social/post/3ludej5ohnk24",
+			desc: "Asimov and Ottilie rest atop a pile of gold.<br/><br/>Gift fr <strong><a href='https://bsky.app/profile/aidenr0.bsky.social'>Spindle</a></strong>.",
 			date_str: "Jul 2025",
 			tags: ["sfw", "anna", "other", "gifts_given"],
 			hidden: false
